9bac164462bf6a4ecf1856387f2f7ff4b68ac5ca
kim dahye <makamong0000@gmail.com>
2024-10-29 09:11:18 +0900

    20241029

--- board_front/src/layouts/Container/index.tsx
+++ board_front/src/layouts/Container/index.tsx
@@ -8,7 +8,6 @@ import Footer from '../Footer';
 : 라우트 계층 구조에서 상위 라우트의 자식 컴포넌트 렌더링 역할
 - Outlet이 위치한 곳에 자식 라우트가 표시
 - 상위 라우트의 레이아웃을 공통으로 사용 + 자식 라우트만의 컴포넌트를 렌더링
-
 */
 
 export default function Container() {
--- board_front/src/react-study/C_StateEffect.tsx
+++ board_front/src/react-study/C_StateEffect.tsx
@@ -1,63 +1,117 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+
 /*
-Menu 카테고리 검색
+  Menu 카테고리 검색 
+  
+  ! Menu 객체 구조
+  - 고유값 id (Long - number)
+  - 메뉴명 name
+  - 메뉴 설명 description
+  - 메뉴 가격 price (number)
+  - 메뉴 이용 가능 여부 isAvailable (boolean)
+  - 메뉴 카테고리 category
+  - 메뉴 사이즈 size
 
-! Menu객체 구조 
-- 고유값 id - Long / number
-- 메뉴명 name - string
-- 메뉴 설명 description - string
-- 메뉴 가격 price - number
-- 메뉴 이용 가능 여부 isAvailable -  boolean
-- 메뉴 카테고리 category - string
-- 메뉴 사이즈 size - string
-
-! HTTP
-메서드: GET
-경로로 : http://localhost:8080/api/v1/menu/search/category
+  ! HTTP 
+  - 메서드: GET
+  - 경로: http://localhost:8080/api/v1/menus/search/category
 */
 
-const DOMAIN = "http://localhost:8080";
-const MENU_API = "api/v1/menus";
+const DOMAIN = 'http://localhost:8080';
+const MENU_API = 'api/v1/menus';
 
-interface getMenuCategoryResponseDto {
-  name: string,
-  description: string,
-  price: number,
-  isAvailable: boolean,
-  category: string,
-  size: string
-} 
+interface GetMenuCategoryResponseDto {
+  name: string;
+  description: string;
+  price: number;
+  isAvailable: boolean;
+  category: string;
+  size: string;
+}
 
-export default function StateEffect() {
+
+type Category = 'Food' | 'Drink' | 'Dessert';
+
+export default function C_StateEffect() {
+  //! 자습) 버튼 클릭으로 필터링 구현하기
+  const [query, setQuery] = useState<Category>('Food');
+
   const [category, setCategory] = useState<string>('');
-  const [results, setResults] = useState<any[]>([]);
-   
+  const [results, setResults] = useState<GetMenuCategoryResponseDto[]>([]);
+  
   const handleCategoryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCategory(e.target.value);
   }
 
-  const fetchMenuData = async(category: string) => {
-    if(category.trim()) {
+  const fetchMenuData = async (category: string) => {
+    if (category.trim()) {
       try {
+
         const response = await axios.get(
           `${DOMAIN}/${MENU_API}/search/category`,
-          { params: { category } }
-        )
+          { params: { category }}
+        );
+
         const data = response.data.data;
+
         setResults(data);
+
       } catch (error) {
-        console.error("Error fetching data", error);
+        console.error("Error fetching data: ", error);
       }
     }
   }
 
+  //! 자습) 버튼 클릭으로 필터링 구현하기
+  //! 매개변수 query 변경
+  const fetchMenuButtonData = async (query: Category) => {
+      try {
+        const response = await axios.get(
+          `${DOMAIN}/${MENU_API}/search/category`,
+          //! params 값 변경
+          { params: { query }}
+        );
+
+        const data = response.data.data;
+
+        setQuery(data);
+
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+  }
+
   useEffect(() => {
     fetchMenuData(category);
-  },[category])
+  }, [category]);
+
+  useEffect(() => {
+    fetchMenuButtonData(query);
+  }, [query]);
+
+  const handleButtonClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+    const selectedCategory = e.currentTarget.value as Category;
+    setQuery(selectedCategory);
+  }
 
   return (
     <div>
+      <div>
+        <button 
+          value='Food'
+          onClick={handleButtonClick}
+        >Food</button>
+        <button 
+          value='Drink' 
+          onClick={handleButtonClick}
+        >Drink</button>
+        <button 
+          value='Dessert' 
+          onClick={handleButtonClick}
+        >Dessert</button>
+      </div>
+
       <input 
         type="text"
         value={category}
@@ -65,6 +119,7 @@ export default function StateEffect() {
         placeholder='Enter Category'
         required
       />
+
       <ul>
         {results.map((result, index) => (
           <li key={index}>{result.name}</li>
